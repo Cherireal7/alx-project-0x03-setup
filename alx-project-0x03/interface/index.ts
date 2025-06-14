55d2cd89262b0b84a0ca5a4d7356e3ffc80b29de
Cherinet <cheridemeke7@gmail.com>
2025-06-14 22:34:13 +0300

    iniitia commit

---
+++ alx-project-0x03/interface/index.ts
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+
+interface PageRouteProps {
+  pageRoute: string
+}
+
+interface ButtonProps {
+  buttonLabel: string
+  buttonSize?: string
+  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
+  action?: () => void
+}
+
+interface LayoutProps {
+  children: ReactNode;
+}
